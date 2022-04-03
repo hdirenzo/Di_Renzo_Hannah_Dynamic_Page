f3cd2335a8cb5b84aadbe8de550f557ca27e31ae
Hannah Di Renzo <hdirenzo@outlook.com>
2022-04-02 20:23:59 -0800

    javascript, click funtion, adding hero img, css placement

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,29 @@
-function openImg() {
-    var image = document.getElementById('image');
-    var source = image.src;
-    window.open(source);
-}
+let swipehannah = document.querySelector(".swipehannah a")
+let swipebirds = document.querySelector(".swipebirds a")
+let swipekitty = document.querySelector(".swipekitty a")
+let swipemocha = document.querySelector(".swipemocha a")
+
+let imghannah = document.querySelector("#imghannah")
+let imgbirds = document.querySelector("#imgbirds")
+let imgkitty = document.querySelector("#imgkitty")
+let imgmocha = document.querySelector("#imgmocha")
+
+swipehannah.addEventListener("click", function (e) {
+    e.preventDefault();
+    imghannah.classList.toggle("hidden")
+});
+
+
+swipebirds.addEventListener("click", function (e) {
+    e.preventDefault();
+    imgbirds.classList.toggle("hidden")
+});
+swipekitty.addEventListener("click", function (e) {
+    e.preventDefault();
+    imgkitty.classList.toggle("hidden")
+});
+swipemocha.addEventListener("click", function (e) {
+    e.preventDefault();
+    imgmocha.classList.toggle("hidden")
+});
+
